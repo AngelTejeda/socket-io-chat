d9531132d7f74823a37bd15c14c4f4cdae436332
Ángel Tejeda <angel.tejedat@gmail.com>
2022-06-24 02:26:45 -0500

    Add message timestamp

--- public/script.js
+++ public/script.js
@@ -1,26 +1,45 @@
-var socket = io();
+// Socket
+let socket = io();
 
+socket.on('new-message', (data) => {
+  if (data.user != username) {
+    generateMessage(data);
+  }
+});
+
+function emitMessage(email, user) {
+  socket.emit('user-data', {
+    mail: email,
+    user: user
+  });
+}
+
+class Message {
+  constructor(user, text, date) {
+    this.user = user;
+    this.text = text;
+    this.date = date;
+  }
+}
+
+// Login
 let loginSection = $("#login");
-let messagesSection = $("#messages");
-
 let user = $("#user");
 let mail = $("#mail");
+let username;
 
+// Chat
+let messagesSection = $("#messages");
+let history = $("#history");
 let messageField = $("#message-field");
 let sendButton = $("#send-button");
 
-let history = $("#history");
-
 let messages = [];
-
-let username;
 
 messagesSection.hide();
 
-// TODO: Remove
-// loginSection.hide();
-// username = "angel";
 
+// Message field events
 messageField.on("input", () => {
   let disabled = messageField.val() == "";
   sendButton.prop("disabled", disabled);
@@ -33,27 +52,61 @@ messageField.on("keypress", (event) => {
   }
 });
 
+
+
+
 function getLastMessage() {
   return messages.length == 0
     ? null
     : messages[messages.length - 1];
 }
 
+// DOM
 function getLastMessageGroup() {
   return $(".message-group").last();
 }
+
 
 function generateMessage(message) {
   let lastMessage = getLastMessage();
 
   let messageGroup;
 
-  console.log(lastMessage);
-  console.log(message);
+  // Timestamp
+  if (lastMessage === null || (message.date - lastMessage.date > 5 * 1000)) {
+    let timeDiff = lastMessage !== null
+      ? message.date - lastMessage.date
+      : Date.now() - message.date;
 
-  if (lastMessage != null && lastMessage.user == message.user
-     && (Date.now() - lastMessage.date) <= 2 * 1000
-    ) {
+    let d = new Date(timeDiff);
+
+    let timestring;
+
+    let hours = timeDiff / 1000 / 60 / 60;
+    let days = hours / 24;
+
+    console.log(hours, days);
+
+    if (hours < 24) {
+      timestring = moment(message.date).format("h:mm");
+      // timestring = d.toLocaleString('en-US', { hour12: false });
+    }
+    else if (days < 24 * 7) {
+      timestring = moment(message.date).format("dddd h:mm")
+    }
+    else {
+      timestring = moment(message.date).format("MMM Do h:mm")
+    }
+
+    let timeElement = $("<div></div>")
+      .text(timestring)
+      .addClass("timestamp");
+
+    history.append(timeElement);
+  }
+
+  // Message Group
+  if (lastMessage != null && lastMessage.user == message.user && (message.date - lastMessage.date) <= 2 * 1000) {
     messageGroup = getLastMessageGroup();
   }
   else {
@@ -67,6 +120,7 @@ function generateMessage(message) {
     history.append(messageGroup);
   }
 
+  // Message
   let newMessage = $("<div></div>")
     .text(message.message)
     .addClass("message");
@@ -74,15 +128,14 @@ function generateMessage(message) {
   messageGroup.append(newMessage);
 
   messages.push(message);
+
+  history.animate({ scrollTop: history.prop("scrollHeight")}, 1000);
 }
 
 function login() {
-  socket.emit('user-data', {
-    mail: mail.val(),
-    user: user.val()
-  });
-
   username = user.val();
+
+  emitMessage(mail.val(), username);
 
   loginSection.hide();
   messagesSection.show();
@@ -94,6 +147,12 @@ function login() {
       generateMessage(data);
     }
   });
+
+  // generateMessage({
+  //   message: "Test",
+  //   date: new Date("2022/06/22"),
+  //   user: "a"
+  // });
 }
 
 function sendMessage() {
@@ -116,9 +175,3 @@ function sendMessage() {
   messageField.val("");
   sendButton.prop("disabled", "true");
 }
-
-socket.on('new-message', (data) => {
-  if (data.user != username) {
-    generateMessage(data);
-  }
-});
